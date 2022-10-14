85795467867d471d80bdedf2ff571e1082f52b46
gerardomp18 <gerardomarinparra18@gmail.com>
2022-10-14 16:53:10 -0500

    [feat] Adding new store procedure that insert new user

--- database/procedure/sp_add_user.sql
+++ database/procedure/sp_add_user.sql
@@ -1,29 +1,21 @@
-USE gbs_dev_db;
+-- SP for the create new user
 
-DROP PROCEDURE IF EXISTS sp_update_user;
+use gbs_dev_db;
+
+DROP PROCEDURE IF EXISTS sp_add_user;
 DELIMITER $$
-CREATE PROCEDURE sp_update_user(
-    IN p_id INT,
-    IN p_first_name    VARCHAR(50),
-    IN p_document_type INT,
-    IN p_last_name VARCHAR(50),
-    IN p_email VARCHAR(120),
-    IN p_password VARCHAR(40),
-    IN p_number_document INT,
-    IN p_phone INT,
-    IN p_birth_date DATE
+CREATE PROCEDURE sp_add_user(
+    IN first_name    VARCHAR(50),
+    IN document_type INT,
+    IN last_name VARCHAR(50),
+    IN email VARCHAR(120),
+    IN password VARCHAR(40),
+    IN number_document INT,
+    IN phone INT,
+    IN birth_date DATE
 )
 BEGIN
-    UPDATE user
-    SET
-    first_name = p_first_name,
-    id_document_type = p_document_type,
-    last_name = p_last_name,
-    email = p_email,
-    password = p_password,
-    number_document = p_number_document,
-    phone = p_phone,
-    birth_date = p_birth_date
-    WHERE id = p_id;
+    INSERT INTO user (first_name,id_document_type,last_name,email,password,number_document,phone,birth_date)
+    VALUE (first_name,document_type,last_name,email,password,number_document,phone,birth_date);
 END $$
 DELIMITER ;
