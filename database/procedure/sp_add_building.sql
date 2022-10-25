d5e1c4040f081e51a7948f3d669687309984a3b3
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-10-25 12:46:12 -0500

    [feat] Updating the procedure that creates a building to accept the new data

--- database/procedure/sp_add_building.sql
+++ database/procedure/sp_add_building.sql
@@ -7,6 +7,9 @@ DELIMITER $$
 CREATE PROCEDURE sp_add_building(
     IN id_condominium INT,
     IN name_building    VARCHAR(120),
+    IN ruc BIGINT,
+    IN phone INT,
+    IN email VARCHAR(120),
     IN description VARCHAR(250),
     IN floor SMALLINT,
     IN address VARCHAR(120),
@@ -14,7 +17,7 @@ CREATE PROCEDURE sp_add_building(
     IN user_updated VARCHAR(60)
 )
 BEGIN
-    INSERT INTO building (id_condominium,name_building,description,floor,address,user_created,user_updated)
-    VALUE (id_condominium,name_building,description,floor,address,user_created,user_updated);
+    INSERT INTO building (id_condominium,name_building,ruc,phone,email,description,floor,address,user_created,user_updated)
+    VALUE (id_condominium,name_building,ruc,phone,email,description,floor,address,user_created,user_updated);
 END $$
 DELIMITER ;
